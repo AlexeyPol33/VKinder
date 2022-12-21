6a0917b4dd164d66e979e06a998c269eb215c24c
Alexey Poltavskiy <79776177342@yandex.ru>
2022-12-21 23:39:17 +0300

    added basic db functionality

--- DataBase/database.py
+++ DataBase/database.py
@@ -1,8 +1,51 @@
 import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 from model import *
 
-DSN = ""
-engine = sqlalchemy.create_engine(DSN)
+def get_engine (dbname,password,login = 'postgres'):
 
-drop_tables(engine)
-create_tables(engine)
+    DSN = f"postgresql://{login}:{password}@localhost:5432/{dbname}"
+    engine = sqlalchemy.create_engine(DSN)
+    return(engine)
+
+
+class database ():
+    def __init__(self,engine) -> None:
+        self.engin = engine
+        Session = sessionmaker(bind=self.engin)
+        self.session = Session()
+
+    def add_user (self,vk_id:int):
+        result = Users(vk_id = vk_id)
+        self.session.add(result)
+        self.session.commit()
+
+    def add_candidate (self,vk_id:int,city:str,age:int,gender:chr):
+        if age < 18:
+            return 'Возраст не может быть меньше 18'
+        if gender != 'м' or gender != 'ж':
+            return 'Не верный пол'
+        result = Candidate(vk_id = vk_id,city = city,age = age,gender = gender)
+        self.session.add(result)
+        self.session.commit()
+
+    def add_likes (self,user_id:int,candidate_id:int):
+        result = Likes(user_id = user_id,candidate_id = candidate_id)
+        self.session.add(result)
+        self.session.commit()
+
+    def add_black__lists (self,user_id:int,candidate_id:int):
+        result = BlackLists(user_id = user_id,candidate_id = candidate_id)
+        self.session.add(result)
+        self.session.commit()
+        pass
+    
+    def get_user_id(self,vk_id:int):
+        result = self.session.query(Users).filter(Users.vk_id == vk_id)
+        print(result.all()[0].id)
+
+
+if __name__ == '__main__':
+    engine = get_engine (dbname = '',password = '')
+    create_tables(engine)
+
--- DataBase/model.py
+++ DataBase/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column,Integer, BigInteger,ForeignKey
+from sqlalchemy import Column,Integer, BigInteger,ForeignKey,String,SmallInteger,CHAR
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -7,20 +7,23 @@ class Users (Base):
     __tablename__ = 'users'
 
     id = Column(Integer, autoincrement = True, primary_key = True)
-    vk_id = Column(BigInteger, unique = True)
+    vk_id = Column(BigInteger, unique = True,nullable=False)
 
 class Candidate(Base):
     __tablename__ = 'candidate'
 
     id = Column(Integer, autoincrement = True, primary_key = True)
-    vk_id = Column(BigInteger, unique = True)
+    vk_id = Column(BigInteger, unique = True,nullable=False)
+    city = Column(String)
+    age = Column(SmallInteger)
+    gender = Column(CHAR(1))
 
 class BlackLists(Base):
     __tablename__ = 'black__lists'
 
     id = Column(Integer, autoincrement = True, primary_key = True)
-    user_id = Column(Integer,ForeignKey('users.id'))
-    candidate_id = Column(Integer,ForeignKey('candidate.id'))
+    user_id = Column(Integer,ForeignKey('users.id'),nullable=False)
+    candidate_id = Column(Integer,ForeignKey('candidate.id'),nullable=False)
 
     users = relationship(Users, backref='black__lists')
     candidate = relationship(Candidate, backref='black__lists')
@@ -29,8 +32,8 @@ class Likes(Base):
     __tablename__ = 'likes'
 
     id = Column(Integer, autoincrement = True, primary_key = True)
-    user_id = Column(Integer,ForeignKey('users.id'))
-    candidate_id = Column(Integer,ForeignKey('candidate.id'))
+    user_id = Column(Integer,ForeignKey('users.id'),nullable=False)
+    candidate_id = Column(Integer,ForeignKey('candidate.id'),nullable=False)
     
     users = relationship(Users, backref='likes')
     candidate = relationship(Candidate, backref='likes')
